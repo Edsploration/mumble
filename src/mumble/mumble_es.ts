6aa77a929ba7e55ed600688ad4288b5d9e680b23
Hosted Weblate <hosted@weblate.org>
2022-03-12 13:56:46 +0100

    TRANSLATION: Update Spanish translation
    Currently translated at 89.8% (1875 of 2086 strings)
    
    Co-authored-by: Allan Nordhøy <epost@anotheragency.no>
    Co-authored-by: Hosted Weblate <hosted@weblate.org>
    Translate-URL: https://hosted.weblate.org/projects/mumble/mumble-client/es/
    Translation: Mumble/Mumble Client

--- src/mumble/mumble_es.ts
+++ src/mumble/mumble_es.ts
@@ -8270,8 +8270,8 @@ Una credencial de acceso es una cadena de texto que puede ser usada como contras
     <message numerus="yes">
         <source>Registered users: %n account(s)</source>
         <translation>
-            <numerusform>Usuarios registrados: %n cuenta(s)</numerusform>
-            <numerusform>Usuarios registrados: %n cuenta(s)</numerusform>
+            <numerusform>Usuarios registrados: %n cuenta</numerusform>
+            <numerusform>Usuarios registrados: %n cuentas</numerusform>
         </translation>
     </message>
     <message>
